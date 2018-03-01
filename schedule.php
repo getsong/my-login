2e47106312af0ca977365b26a46c6a19685b202c
getsong <getsong13@gmail.com>
2018-03-02 00:15:46 +0800

    automated table rowspan

--- schedule.php
+++ schedule.php
@@ -4,7 +4,12 @@
     <title>Your Schedule</title>
     <style>
         table, th, td {
-            border: 2px solid;
+            border: 2px blueviolet solid;
+            text-align: center;
+            color: #202020;
+        }
+        th, td {
+            width: 100px;
         }
     </style>
 </head>
@@ -13,16 +18,96 @@
     <h3>Enter your schedule below</h3>
     <form method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
         <div>
-            <textarea rows="8" cols="70" name="scheduleData"></textarea>
+            <select name="day">
+                <?php
+                $days = ["Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"];
+                for ($day = 0; $day < 6; $day++) {
+                    echo "<option value='$day'>" . $days[$day] . "</option>";
+                }
+                ?>
+            </select>
+            <select name="startTime">
+                <?php
+                for ($slot = 0; $slot < 20; $slot++) {
+                    if ($slot % 2 === 0) {
+                        echo "<option value='$slot'>" . (8 + intdiv($slot, 2)) . ":30";
+                    }
+                    else {
+                        echo "<option value='$slot'>" . (9 + intdiv($slot, 2)) . ":00";
+                    }
+                }
+                ?>
+            </select>
+            <select name="endTime">
+                <?php
+                for ($slot = 1; $slot < 21; $slot++) {
+                    if ($slot % 2 === 0) {
+                        echo "<option value='$slot'>" . (9 + intdiv($slot, 2)) . ":00";
+                    }
+                    else {
+                        echo "<option value='$slot'>" . (9 + intdiv($slot, 2)) . ":30";
+                    }
+                }
+                ?>
+            </select>
+            <!--textarea rows="8" cols="70" name="scheduleData"></textarea-->
         </div>
         <div>
             <button type="submit">submit</button>
         </div>
     </form>
     <?php
-        echo "schedule data: $_POST[scheduleData]";
+    $duration = array_fill(0, 6, array_fill(0, 20, 1));
+    print_r($duration);
+    echo "<br>";
+    $input = array_fill(0, 6, array_fill(0, 20, 0));
+    $input[0][1] = 2;
+    $input[0][2] = -1;
+    $input[0][3] = -1;
+    for ($day = 0; $day < 6; $day++) {
+        for ($slot = 0; $slot < 20; $slot++) {
+            $duration[$day][$slot] += $input[$day][$slot];
+        }
+    }
+    print_r($duration);
+    echo "<br>";
     ?>
     <table>
+        <tr>
+            <th>Time/Day</th>
+            <th>Mon</th>
+            <th>Tue</th>
+            <th>Wed</th>
+            <th>Thurs</th>
+            <th>Fri</th>
+            <th>Sat</th>
+        </tr>
+        <?php
+        for ($slot = 0; $slot < 20; $slot++) {
+            echo "<tr>";
+            for ($day = 0; $day < 7; $day++) {
+                if ($day == 0) {
+                    if ($slot % 2 == 0) {
+                        echo "<th>" . (8 + intdiv($slot, 2)) . ":30-" . (9 + intdiv($slot, 2)) . ":00</th>";
+                    }
+                    else {
+                        echo "<th>" . (9 + intdiv($slot, 2)) . ":00-" . (9 + intdiv($slot, 2)) . ":30</th>";
+                    }
+                }
+                else if ($duration[$day - 1][$slot] > 0) {
+                    echo "<td rowspan=\"{$duration[$day - 1][$slot]}\">" . $duration[$day - 1][$slot] . "</td>";
+                }
+            }
+            echo "</tr>";
+        }
+        //echo "schedule data: $_POST[scheduleData]";
+        ?>
+    </table>
+    <?php
+    echo "_POST: ";
+    print_r($_POST);
+    ?>
+    <!--<table>
         <tr>
             <th>Time/Day</th>
             <th>Mon</th>
@@ -43,7 +128,7 @@
         </tr>
         <tr>
             <th>0930-1030</th>
-            <td rowspan="3">a</td>
+            <td rowspan="0">a</td>
             <td>a</td>
             <td>a</td>
             <td>a</td>
@@ -77,12 +162,12 @@
         </tr>
         <tr>
             <th>1330-1430</th>
-            <td>a</td>
-            <td>a</td>
-            <td>a</td>
-            <td>a</td>
-            <td>a</td>
-            <td>a</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
         </tr>
         <tr>
             <th>1430-1530</th>
@@ -93,6 +178,6 @@
             <td>a</td>
             <td>a</td>
         </tr>
-    </table>
+    </table>-->
 </body>
 </html>
